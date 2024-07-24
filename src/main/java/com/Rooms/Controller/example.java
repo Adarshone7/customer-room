ca87209699d5c0cf6507b6916dbcffea8d7e6d2f
adarsh <pujariadarsh4@gmail.com>
2024-07-24 22:21:40 +0530

    defect is fixed

---
+++ src/main/java/com/Rooms/Controller/example.java
@@ -0,0 +1,10 @@
+package com.Rooms.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class example {
+    public String getnmae(){
+        return "adsrsh";
+    }
+}
